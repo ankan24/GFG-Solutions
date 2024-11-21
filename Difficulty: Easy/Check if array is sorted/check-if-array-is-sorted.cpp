e1d19d43adb912d27bfcbc3f32cb849bb393bd4d
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2024-11-22 00:09:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.cpp
+++ Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.cpp
@@ -11,14 +11,13 @@ class Solution {
     bool arraySortedOrNot(vector<int>& arr) {
         // code here
         int n = arr.size();
-        int flag = 0;
         for(int i=0;i<n-1;i++){
-            if(arr[i] <= arr[i+1]){
-                flag++;
+            if(arr[i] > arr[i+1]){
+                return false;
             }
         }
-        if(flag == n-1) return true;
-        else return false;
+     return true;
+         
     }
 };
 
