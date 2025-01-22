6fb8d6ee31c75364cc864d712b920f2a30151e0c
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-01-22 19:58:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -12,7 +12,7 @@ class Solution {
         int n = arr.size();
         for(int i=0;i<=n-1;i++){
             int j = i;
-             while(j >= 0 && arr[j-1] > arr[j]){
+             while(j  > 0 && arr[j-1] > arr[j]){
                  swap(arr[j-1],arr[j]);
                  j--;
              }
