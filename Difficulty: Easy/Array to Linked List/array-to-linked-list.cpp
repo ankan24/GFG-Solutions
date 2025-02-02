aff7a8b8e6c3ea58fb36dc1d2fe8a3ddfb1381e0
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-02-02 14:17:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
@@ -46,23 +46,22 @@ public:
     }
 };*/
 
+
 class Solution {
   public:
     Node* constructLL(vector<int>& arr) {
         // code here
         int n = arr.size();
-       Node* temp = new Node();
-       Node* head = temp;
-       for(int i=0;i<n;i++){
-           temp->data = arr[i];
-           if(i<n-1){
-           temp->next = new Node();
+        Node* head = new Node(arr[0]);
+        Node* temp = head;
+        for(int i=1;i<n;i++){
+           temp->next = new Node(arr[i]);
            temp = temp->next;
-           }
-       }
-       return head; 
+        }
+        return head;
     }
 };
+
 
 //{ Driver Code Starts.
 
