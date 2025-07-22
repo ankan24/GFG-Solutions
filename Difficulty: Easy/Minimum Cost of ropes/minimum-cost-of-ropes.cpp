e4174b992696551243ac89e085193bae71742495
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-22 19:09:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        // code here
+        priority_queue<int,vector<int>, greater<int>> pq;
+        
+        for(int i=0;i<arr.size();i++){
+            pq.push(arr[i]);
+        }
+        
+        int ans = 0;
+        while(pq.size() > 1){
+            int f = pq.top();
+            pq.pop();
+            int s = pq.top();
+            pq.pop();
+            ans += f+s;
+            pq.push(f+s);
+        }
+        return ans;
+    }
+};
