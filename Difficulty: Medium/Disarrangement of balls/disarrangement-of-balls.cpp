50d7192251d6b265715de6e709d9f83aea9829ea
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-31 00:58:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Disarrangement of balls/disarrangement-of-balls.cpp
@@ -0,0 +1,12 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int countDer(int n) {
+        if(n==1) return 0;
+        if(n==2) return 1;
+        
+        int ans = (n-1)*(countDer(n-1)+countDer(n-2));
+        return ans;
+    }
+};
